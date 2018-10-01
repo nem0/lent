40e2101c4ca11fde811a758aa02de20dea92e0a0
Mikulas Florek <mikulas.florek@gamedev.sk>
2018-10-01 20:11:31 +0200

    header guards

--- lent.h
+++ lent.h
@@ -1,5 +1,24 @@
 #pragma once
 
+// this is Imperial Royal Guard guarding this header
+// 
+//     |  __
+//     | /__\
+//     | X~~|
+//     |-\|//-.
+//    /|`.|'.' \
+//   |,|.\~~ /||
+//   |:||   ';||
+//   ||||   | ||
+//   \ \|     |`.
+//   |\X|     | |
+//   | .'     |||
+//   | |   .  |||
+//   |||   |  `.|
+//   ||||  |   ||
+//   ||||  |   ||
+//   `+.__._._+~' LS
+//
 
 // entity 
 typedef unsigned int lent_entity;
