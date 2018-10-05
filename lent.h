b7f70280cc69522b7ea81f7c0d78ea935b50e495
Mikulas Florek <mikulas.florek@gamedev.sk>
2018-10-05 21:32:08 +0200

    YOLOgit add .!

--- lent.h
+++ lent.h
@@ -1,24 +1,27 @@
 #pragma once
 
-// this is Imperial Royal Guard guarding this header
-// 
-//     |  __
-//     | /__\
-//     | X~~|
-//     |-\|//-.
-//    /|`.|'.' \
-//   |,|.\~~ /||
-//   |:||   ';||
-//   ||||   | ||
-//   \ \|     |`.
-//   |\X|     | |
-//   | .'     |||
-//   | |   .  |||
-//   |||   |  `.|
-//   ||||  |   ||
-//   ||||  |   ||
-//   `+.__._._+~' LS
-//
+#ifdef IMPERIAL_ROYAL_GUARD
+this is Imperial Royal Guard guarding this header
 
-// entity 
+    |  __
+    | /__\
+    | X~~|
+    |-\|//-.
+   /|`.|'.' \
+  |,|.\~~ /||
+  |:||   ';||
+  ||||   | ||
+  \ \|     |`.
+  |\X|     | |
+  | .'     |||
+  | |   .  |||
+  |||   |  `.|
+  ||||  |   ||
+  ||||  |   ||
+  `+.__._._+~' LS
+#endif
+
+#ifdef ENTITY_COMMENT 
+entity 
+#endif
 typedef unsigned int lent_entity;
